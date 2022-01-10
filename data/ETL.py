576ee23252694d2c05b9736a3b06f257e66aa52b
Mantas <pieza.mantas@gmail.com>
2022-01-10 20:09:29 +0200

    finished etl class. needs logging

--- data/ETL.py
+++ data/ETL.py
@@ -50,8 +50,10 @@ class ETL:
             with open(upload_file_path, "rb") as data:
                 blob_client.upload_blob(data)
 
-    def extract_single_taxi_blob(self, file_name: str, file_content):
-        local_blob_path = "extracted_from_azure"
+    def extract_single_taxi_blob(
+        self, file_name: str, file_content, local_blob_path: str
+    ):
+
         download_file_path = os.path.join(local_blob_path, file_name)
 
         with open(download_file_path, "wb") as file:
@@ -62,28 +64,33 @@ class ETL:
             "raw-yellow-taxi-data"
         )
         list_of_blobs = my_container.list_blobs()
-
+        local_blob_path = "extracted_from_azure_raw"
         for blob in list_of_blobs:
             single_csv_file = (
                 my_container.get_blob_client(blob).download_blob().readall()
             )
-            self.extract_single_taxi_blob(blob.name, single_csv_file)
+            self.extract_single_taxi_blob(blob.name, single_csv_file, local_blob_path)
 
     def transform_raw_data(self, start_month: int, end_month: int, year: int):
         data_processing = Data_processing(start_month, end_month, year)
         data_processing.run()
 
     def upload_transformed_data_to_azure(
-        self, start_month: int, end_month: int, year: str = "2021"
+        self, start_month: int, end_month: int, year: int
     ):
-        container_name = "transformed-raw-yellow-taxi-data"
-        self.blob_service_client.create_container(container_name)
+        container_name = "transformed-yellow-taxi-data"
+        try:
+            self.blob_service_client.create_container(container_name)
+        except:
+            self.blob_service_client.get_container_client(container_name)
         raw_data_file_path = "transformed_data"
         months = [str(i).zfill(2) for i in range(start_month, end_month + 1)]
 
         for month in months:
 
-            temp_filename = f"clean_yellow_tripdata_{year}-" + str(month) + ".csv"
+            temp_filename = (
+                "clean_yellow_trip_data_" + str(year) + "-" + str(month) + ".csv"
+            )
             blob_client = self.blob_service_client.get_blob_client(
                 container=container_name, blob=temp_filename
             )
@@ -92,3 +99,36 @@ class ETL:
             upload_file_path = os.path.join(raw_data_file_path, temp_filename)
             with open(upload_file_path, "rb") as data:
                 blob_client.upload_blob(data)
+
+    def extract_transformed_taxi_data_from_azure(self):
+        my_container = self.blob_service_client.get_container_client(
+            "transformed-yellow-taxi-data"
+        )
+        list_of_blobs = my_container.list_blobs()
+        local_blob_path = "extracted_from_azure_transformed"
+        for blob in list_of_blobs:
+            single_csv_file = (
+                my_container.get_blob_client(blob).download_blob().readall()
+            )
+            self.extract_single_taxi_blob(blob.name, single_csv_file, local_blob_path)
+
+    def delete_container(self, container_name: str):
+        my_container = self.blob_service_client.get_container_client(
+            container=container_name
+        )
+
+        my_container.delete_container()
+
+    def delete_blob(self, container_name: str, blob_name: str):
+        my_container = self.blob_service_client.get_container_client(
+            container=container_name
+        )
+
+        my_container.delete_blob(blob_name)
+
+
+etl = ETL()
+
+etl.upload_transformed_data_to_azure(7, 7, 2021)
+
+# etl.delete_blob(container_name='transformed-yellow-taxi-data' ,blob_name='clean_yellow_trip_data_2021-07.csv')
