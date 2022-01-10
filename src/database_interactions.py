957f058ebb825231c64bf5f3cdf5bed7cf098488
Mantas <pieza.mantas@gmail.com>
2022-01-11 00:07:21 +0200

    created requirements txt

--- src/database_interactions.py
+++ src/database_interactions.py
@@ -14,7 +14,7 @@ server_name = "yellowtaxidata"
 database_name = "yellow_taxi_database_2021"
 
 
-server = f"yellowtaxidata.database.windows.net,1433"
+server = "yellowtaxidata.database.windows.net,1433"
 username = os.getenv("USERNAME")
 password = os.getenv("PASSWORD")
 
@@ -25,14 +25,14 @@ connection_string = textwrap.dedent(
     Database={database_name};
     Uid={username};
     Pwd={password};
-    Encrupt=yes;
+    Encrypt=yes;
     TrustServerCertificate=no;
     Connection Timeout=30;
 """
-pyodbc.connect
+)
 # create a new PYODBC connectio object
 
-conn = pyodbc.connect(connection_string)
+cntc: pyodbc.Connection = pyodbc.connect(connection_string)
 
 # pyodbc.Cursor = cnxn.cursor()
 # # define select query
@@ -43,5 +43,5 @@ conn = pyodbc.connect(connection_string)
 # # """
 # # crsr.execute(select_sql)
 
-conn.close()
+cntc.close()
 # print(driver)
